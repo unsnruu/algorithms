dcff9bcc05d7a0f44d58bf3d998f81c952ee2784
unsnruu <unsnruu@gmail.com>
2022-05-05 07:45:34 +0900

    Declare source page

--- problems/220505/BOJ-1463.js
+++ problems/220505/BOJ-1463.js
@@ -1,25 +1,26 @@
 ///https://www.acmicpc.net/problem/1463
+// 풀이 참고: https://hongcoding.tistory.com/46
 
 // const fs = require("fs");
 // let input = fs.readFileSync("/dev/stdin").toString().trim();
 // input = +input;
 
-function solution(X) {
-  if (X === 1) return 0;
-  //dp는 X를 구하는 최소한의 연산 사용 횟수
-  const dp = [0, 0];
-  for (let i = 2; i <= X; i++) {
-    dp[i] = dp[i - 1] + 1;
-    if (i % 2 === 0) {
-      dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-    }
-    if (i % 3 === 0) {
-      dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-    }
-  }
-  return dp;
-}
-solution(10);
+// function solution(X) {
+//   if (X === 1) return 0;
+//   //dp는 X를 구하는 최소한의 연산 사용 횟수
+//   const dp = [0, 0];
+//   for (let i = 2; i <= X; i++) {
+//     dp[i] = dp[i - 1] + 1;
+//     if (i % 2 === 0) {
+//       dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+//     }
+//     if (i % 3 === 0) {
+//       dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+//     }
+//   }
+//   return dp;
+// }
+// solution(10);
 
 //bruet force 실패 = StackSizeExceeded
 // function solution(num) {
